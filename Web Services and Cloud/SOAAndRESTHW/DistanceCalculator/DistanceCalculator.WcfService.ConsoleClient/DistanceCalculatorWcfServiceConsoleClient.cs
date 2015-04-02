65482b30940f9bbb79ca3b434d33ef0d9a4b117e
ttitto <ttitto_prog@vfemail.net>
2015-04-02 08:58:28 +0300

    Wcf Service Host and Console Client implemented

---
+++ Web Services and Cloud/SOAAndRESTHW/DistanceCalculator/DistanceCalculator.WcfService.ConsoleClient/DistanceCalculatorWcfServiceConsoleClient.cs
@@ -0,0 +1,18 @@
+﻿namespace DistanceCalculator.WcfService.ConsoleClient
+{
+    using System;
+
+    using DistanceCalculator.Models;
+    using DistanceCalculator.WcfService.ConsoleClient.DistanceCalculatorService;
+
+    class DistanceCalculatorWcfServiceConsoleClient
+    {
+        static void Main(string[] args)
+        {
+            DistanceCalculatorServiceClient client = new DistanceCalculatorServiceClient();
+            Point startPoint = new Point() { X = 2, Y = 3 };
+            Point endPoint = new Point() { X = 4, Y = 5 };
+            Console.WriteLine("The calculated distance is {0:0.00}", client.CalcDistance(startPoint, endPoint));
+        }
+    }
+}
---
+++ Web Services and Cloud/SOAAndRESTHW/DistanceCalculator/DistanceCalculator.WcfService.Host/DistanceCalculatorWcfServiceHost.cs
@@ -0,0 +1,28 @@
+﻿namespace DistanceCalculator.WcfService.Host
+{
+    using System;
+    using System.ServiceModel;
+    using System.ServiceModel.Description;
+
+    class DistanceCalculatorWcfServiceHost
+    {
+        static void Main(string[] args)
+        {
+            Uri serviceAddress = new Uri("http://localhost:4321/calc");
+            ServiceHost distanceCalculatorSelfHost = new ServiceHost(typeof(DistanceCalculatorService), serviceAddress);
+            ServiceMetadataBehavior distanceCalculatorSmb = new ServiceMetadataBehavior();
+
+            distanceCalculatorSmb.HttpGetEnabled = true;
+            distanceCalculatorSelfHost.Description.Behaviors.Add(distanceCalculatorSmb);
+
+            using (distanceCalculatorSelfHost)
+            {
+                distanceCalculatorSelfHost.Open();
+                Console.WriteLine("The distance calulator service was started at endpoint {0}", serviceAddress);
+
+                Console.WriteLine("Press [Enter] to exit:");
+                Console.ReadLine();
+            }
+        }
+    }
+}
