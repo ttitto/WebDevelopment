3509b5d299462aafb38ca87db6a096f7cc8459b4
ttitto <ttitto_prog@vfemail.net>
2015-04-03 20:09:46 +0300

    client for WebApi service written

---
+++ Web Services and Cloud/SOAAndRESTHW/DistanceCalculator/DistanceCalculator.WebApiService.Client/DistanceCalculatorWebApiClient.cs
@@ -0,0 +1,37 @@
+﻿namespace DistanceCalculator.WebApiService.Client
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class DistanceCalculatorWebApiClient
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:8856/")
+            };
+
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync("api/distancecalculator?ax=2&ay=3&bx=4&by=5").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var distance = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(distance);
+            }
+            else
+            {
+                Console.WriteLine("{0} {1}", response.StatusCode, response.ReasonPhrase);
+            }
+
+            Console.WriteLine("Press [Enter] to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Web Services and Cloud/SOAAndRESTHW/DistanceCalculator/DistanceCalculator.WebApiService.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DistanceCalculator.WebApiService.Client")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DistanceCalculator.WebApiService.Client")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a3eebe4e-48fd-4806-b036-2a1e5fc58446")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
